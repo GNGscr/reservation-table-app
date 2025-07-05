4738366b2dd70dc155786f20563c0fc89bb4a25c
GNGscr <de.brand808@gmail.com>
2025-07-05 22:48:55 +0300

    clean comments

--- client/src/AppWrapper.tsx
+++ client/src/AppWrapper.tsx
@@ -1,8 +1,8 @@
 import { useMemo, useState, useEffect } from 'react';
 import { ThemeProvider, CssBaseline } from '@mui/material';
-import App from './App';
-import { getAppTheme } from './theme';
 import ThemeToggle from './components/ThemeToggle';
+import { getAppTheme } from './theme';
+import App from './App';
 
 const AppWrapper = () => {
 
@@ -37,34 +37,3 @@ const AppWrapper = () => {
 };
 
 export default AppWrapper;
-
-// const AppWrapper = () => {
-//     const [mode, setMode] = useState<'light' | 'dark'>('light');
-  
-//     useEffect(() => {
-//       const saved = localStorage.getItem('theme') as 'light' | 'dark' | null;
-//       if (saved === 'light' || saved === 'dark') {
-//         setMode(saved);
-//       } else {
-//         const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-//         setMode(prefersDark ? 'dark' : 'light');
-//       }
-//     }, []);
-  
-//     useEffect(() => {
-//       localStorage.setItem('theme', mode);
-//     }, [mode]);
-  
-//     const theme = useMemo(() => getAppTheme(mode), [mode]);
-  
-//     return (
-//       <ThemeProvider theme={theme}>
-//         <CssBaseline />
-//         <div style={{ position: 'absolute', top: 10, right: 10 }}>
-//           <ThemeToggle mode={mode} setMode={setMode} />
-//         </div>
-//         <App />
-//       </ThemeProvider>
-//     );
-//   };
-  
