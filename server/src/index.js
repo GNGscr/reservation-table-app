1c1bec582b0c311e40fca00b076f95f7c58c45f6
GNGscr <de.brand808@gmail.com>
2025-10-07 19:26:42 +0300

    setup vercal deploy

--- server/src/index.js
+++ server/src/index.js
@@ -3,13 +3,18 @@ import cors from 'cors';
 import productsRouter from './routes/products.js';
 
 const app = express();
-const PORT = 5000;
+// const PORT = 5000;
 
 app.use(cors());
 app.use(express.json());
 
 app.use('/api/products', productsRouter);
 
-app.listen(PORT, () => {
-    console.log(`Server is listening on http://localhost:${PORT}`);
-});
+
+app.get("/api/products", (req, res) => {
+    res.json({ msg: "Hello from the server" });
+  });
+
+// app.listen(PORT, () => {
+//     console.log(`Server is listening on http://localhost:${PORT}`);
+// });
